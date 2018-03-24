a7cfd321396326477ea164dc553dbe1435d199f2
Serhii Pererva <sergey.pererva@gmail.com>
2018-03-24 22:41:40 +0200

    Command line argument parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,31 @@
 module Main where
 
-import Lib
+import           Data.Semigroup      ((<>))
+import           Options.Applicative
+
+data Action =
+  Add { rawNotes :: String }
+  deriving (Show)
+
+addParser :: Parser Action
+addParser = Add <$> argument str (metavar "REMINDERS")
+
+data Args = Args
+  { action :: Action }
+
+actionParser :: Parser Args
+actionParser =
+  Args <$>
+  hsubparser (command "add" (info addParser (progDesc "Create new notes")))
 
 main :: IO ()
-main = putStrLn "later"
+main = handle =<< execParser opts
+  where
+    opts = info (actionParser <**> helper)
+      ( fullDesc
+     <> progDesc "Create or list remainders in osX Reminders app"
+     <> header "Remainders helper" )
+
+handle :: Args -> IO ()
+handle (Args a) = putStrLn . show $ a
+--handle _             = return ()
