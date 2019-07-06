480de6c755b82ade7e8c071443e7cf5312494d76
Serhii Pererva <serhii.pererva@gmail.com>
2019-07-06 19:31:27 +0100

    Format using brittany

--- app/Main.hs
+++ app/Main.hs
@@ -1,43 +1,50 @@
 module Main where
 
-import           AppleScript      (runAppleScript)
-import           Args             (Action (..), Args (..), arguments,
-                                   execParser)
+import           AppleScript                    ( runAppleScript )
+import           Args                           ( Action(..)
+                                                , Args(..)
+                                                , arguments
+                                                , execParser
+                                                )
 import           Command
-import           Data.Bifunctor   (bimap)
-import           Data.Foldable    (fold)
-import           Data.Text        (pack)
-import           Parser           (reminders, runParser)
+import           Data.Bifunctor                 ( bimap )
+import           Data.Foldable                  ( fold )
+import           Data.Text                      ( pack )
+import           Data.UUID                      ( toText )
+import           Parser                         ( reminders
+                                                , runParser
+                                                )
 import           System.Directory
 import           System.FilePath
-import           System.FSNotify  hiding (Action)
+import           System.FSNotify         hiding ( Action )
+import           System.Random                  ( randomIO )
 import           Types
-import           Data.UUID        (toText)
-import           System.Random    (randomIO)
+
 
 main :: IO ()
 main = handle =<< execParser arguments
-  where
-    handle :: Args -> IO ()
-    handle (Args (Add body)) = do
-      ident <- randomIO
-      runAppleScript . create $ Reminder (pack body) (toText ident) (pack "") (Just Todo)
-    handle (Args (Sync path toWatch)) =
-      if toWatch
-      then withManager $ \mgr -> execute >> do
-        canonPath <- canonicalizePath path
-        stop <-
-          watchDir
-          mgr
-          (takeDirectory path)
-          (equalFilePath canonPath . eventPath)
-          (const execute)
-        putStrLn "Listening for changes..."
-        putStrLn "📝 Press <enter> to stop"
-        _ <- getChar
-        stop
-      else execute
-      where
-        execute =
-          runParser . pack <$> readFile path >>=
-          fold . bimap print (runAppleScript . sync . reminders)
+ where
+
+  handle :: Args -> IO ()
+  handle (Args (Add body)) = do
+    ident <- randomIO
+    runAppleScript . create $ Reminder (pack body) (toText ident) (pack "") (Just Todo)
+  handle (Args (Sync path False)) = syncFile path
+  handle (Args (Sync path True )) = syncFile path >> watchFileChanges path
+
+  syncFile path = runParser . pack <$> readFile path >>= fold . bimap handleError execute
+
+  execute     = runAppleScript . sync . reminders
+
+  handleError = print
+
+  watchFileChanges path = withManagerConf defaultConfig { confThreadPerEvent = False } $ \mgr -> do
+    canonPath <- canonicalizePath path
+    let dir          = takeDirectory path
+        shouldUpdate = equalFilePath canonPath . eventPath
+        onChange     = const (syncFile path)
+    stop <- watchDir mgr dir shouldUpdate onChange
+    putStrLn "Listening for changes..."
+    putStrLn "📝 Press <enter> to stop"
+    _ <- getChar
+    stop
