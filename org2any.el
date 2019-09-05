08930063293c1bd2b24a780611c75ee42748e621
Serhii Pererva <serhii.pererva@gmail.com>
2019-09-05 22:54:45 +0100

    Customization for files to be automatically watched

--- org2any.el
+++ org2any.el
@@ -1,25 +1,46 @@
 ;;; org2any.el --- Integration of org2any utility into Emacs -*- coding: utf-8 -*-
+
+;; Package-Requires: ((dash "2.12.0"))
+
 ;;; Commentary:
 
 ;;; Starts watching org file when it's opened in Emacs.
 
 ;;; Code:
 
+(setq lexical-binding t)
+
+(require 'dash)
+
 (defcustom org2any/executable-path "org2any"
   "Path to org2any executable."
   :type '(file :must-match t)
   :group 'org2any)
 
-(defvar org2any/running-processes)
-(setq org2any/running-processes '())
+(defcustom org2any/autosync-files-regexes '("^.*/?test.org$")
+  "Files for which automatic synchronization should be enabled."
+  :type '(repeat regexp)
+  :group 'org2any)
+
+(defvar org2any/running-processes nil
+  "Mapping from org buffers to running org2any processes.")
 
 (defun org2any/start ()
   "Start org2any in file watch mode."
-  (when (and (string= (file-name-nondirectory (buffer-file-name)) "test.org")
-	     (not (assoc (current-buffer) org2any/running-processes)))
-    (let ((org2any-process
-	   (start-process org2any/executable-path "*org2any log*" "org2any" (buffer-file-name) "-v" "-w")))
-      (setq org2any/running-processes (cons `(,(current-buffer) . ,org2any-process) org2any/running-processes))
+  (when (and
+	 (not (assoc (current-buffer) org2any/running-processes))
+	 (-any-p
+	      (lambda (re) (string-match-p re (buffer-file-name)))
+	      org2any/autosync-files-regexes))
+    (let ((org2any-process (start-process
+			    org2any/executable-path
+			    "*org2any log*"
+			    "org2any"
+			    (buffer-file-name)
+			    "-v"
+			    "-w")))
+      (setq org2any/running-processes
+	    (cons `(,(current-buffer) . ,org2any-process) org2any/running-processes))
       (add-hook 'kill-buffer-hook 'org2any/teardown))
     (message "org2any started for %s" (buffer-name))))
 
