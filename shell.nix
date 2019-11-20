54d9ff74e64c3d53b4ffc1ed489b6cae6f2241cf
Serhii Pererva <serhii.pererva@gmail.com>
2019-11-20 22:28:10 +0000

    Simplify nix shell script to not attempt to build haskell package

--- shell.nix
+++ shell.nix
@@ -1,31 +1,16 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
   inherit (nixpkgs) pkgs;
-
   org2any-emacs = import ./org2any-emacs.nix;
-
-  haskellPackages = pkgs.haskellPackages.override {
-    overrides = self: super: {
-      org2any-cli = self.callPackage ./org2any-cli.nix {};
-    };
-  };
-
-  haskellDeps = ps: with ps; [
-    base
-    org2any-cli
-  ];
-
-  ghc = haskellPackages.ghcWithPackages haskellDeps;
 in
 pkgs.stdenv.mkDerivation {
-  name = "env";
+  name = "dev-env";
   buildInputs = [
-    ghc
+    pkgs.haskellPackages.ghc
+    pkgs.haskellPackages.stack
+    pkgs.haskellPackages.cabal-install
     org2any-emacs
     pkgs.emacs
     pkgs.emacsPackages.dash
-    haskellPackages.org2any-cli
-    haskellPackages.stack
-    haskellPackages.cabal-install
   ];
 }
