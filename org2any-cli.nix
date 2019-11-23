221d3a710937fd3c4c15ef6a90d9600f6d7975a6
Serhii Pererva <serhii.pererva@gmail.com>
2019-11-23 23:25:28 +0000

    Clarify dependencies in nix script

--- org2any-cli.nix
+++ org2any-cli.nix
@@ -1,36 +1,25 @@
-{ mkDerivation, aeson, attoparsec, base, bytestring, containers
+{ mkDerivation, aeson, attoparsec, bytestring, containers
 , directory, fast-logger, filepath, free, fsnotify, hpack, hspec
 , insert-ordered-containers, mtl, optparse-applicative
 , orgmode-parse, QuickCheck, random, stdenv, text, thyme, time
 , typed-process, unix, universum, unordered-containers, uuid
-, stack, Cabal
 }:
 mkDerivation {
   pname = "org2any-cli";
-  version = "0.1.0.0";
+  version = "0.0.1";
   src = ./.;
   isLibrary = false;
+  doCheck = true;
   isExecutable = true;
-  buildDepends = [ stack ];
   libraryHaskellDepends = [
     aeson attoparsec base bytestring containers directory fast-logger
     filepath free fsnotify insert-ordered-containers mtl
-    optparse-applicative orgmode-parse random text thyme time
-    typed-process unix universum unordered-containers uuid
+    orgmode-parse random text thyme time typed-process
+    unordered-containers uuid universum
   ];
   libraryToolDepends = [ hpack ];
-  executableHaskellDepends = [
-    aeson attoparsec base bytestring containers directory fast-logger
-    filepath free fsnotify insert-ordered-containers mtl
-    optparse-applicative orgmode-parse random text thyme time
-    typed-process unix universum unordered-containers uuid
-  ];
-  testHaskellDepends = [
-    aeson attoparsec base bytestring containers directory fast-logger
-    filepath free fsnotify hspec insert-ordered-containers mtl
-    optparse-applicative orgmode-parse QuickCheck random text thyme
-    time typed-process unix universum unordered-containers uuid
-  ];
+  executableHaskellDepends = [ optparse-applicative unix ];
+  testHaskellDepends = [ hspec QuickCheck ];
   prePatch = "hpack";
   homepage = "https://github.com/serhiip/org2any#readme";
   license = stdenv.lib.licenses.bsd3;
