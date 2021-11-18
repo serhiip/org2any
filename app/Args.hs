82bc6f9ed089e0862245f32fb43dd92a99343a88
Serhii Pererva <serhii.pererva@gmail.com>
2021-11-18 21:35:12 +0000

    .

--- app/Args.hs
+++ app/Args.hs
@@ -3,29 +3,36 @@ module Args
   , Action(..)
   , arguments
   , execParser
-  )
-where
+  ) where
 
-import           Universum
+import           Data.OrgMode.Sync.Types        ( SyncConfig(SyncConfig)
+                                                , Verbosity(..)
+                                                )
 import           Options.Applicative
-import           Data.OrgMode.Sync.Types
+import           Universum
 
-data Action
-  = Sync { filePath :: FilePath
-         , syncDestination :: Maybe Text
-         , watch    :: Bool }
-  deriving (Show)
+data Action = Sync
+  { filePath        :: FilePath
+  , syncDestination :: Maybe Text
+  , watch           :: Bool
+  } | Version
+  deriving Show
 
 verboseOpt :: Parser Verbosity
 verboseOpt =
   flag Normal Verbose (short 'v' <> help "Log additional information to stdout")
 
 quietOpt :: Parser Verbosity
-quietOpt =
-  flag' Quiet (long "quiet" <> short 'q' <> help "Dont log any information to stdout")
+quietOpt = flag'
+  Quiet
+  (long "quiet" <> short 'q' <> help "Dont log any information to stdout")
 
-syncParser :: Parser Action
-syncParser =
+version :: Parser Action
+version =
+  flag' Version (long "version" <> short 'v' <> help "Print version and exit")
+
+actionParser :: Parser Action
+actionParser =
   Sync
     <$> argument str (metavar "FILEPATH")
     <*> (   Just
@@ -43,6 +50,7 @@ syncParser =
           (long "watch" <> short 'w' <> help
             "Watch file changes and execute a sync on each change"
           )
+    <|> version
 
 configParser :: Parser SyncConfig
 configParser = SyncConfig <$> (verboseOpt <|> quietOpt)
@@ -50,9 +58,12 @@ configParser = SyncConfig <$> (verboseOpt <|> quietOpt)
 data Args = Args
   { action :: Action
   , config :: SyncConfig
-  } deriving (Show)
+  }
+  deriving Show
 
 arguments :: ParserInfo Args
 arguments = info
-  (Args <$> syncParser <*> configParser <**> helper)
-  (fullDesc <> progDesc "Sync org file with MacOS Reminders" <> header "Reminders helper")
+  (Args <$> actionParser <*> configParser <**> helper)
+  (fullDesc <> progDesc "Sync org file with MacOS Reminders" <> header
+    "Reminders helper"
+  )
--- app/Main.hs
+++ app/Main.hs
@@ -51,51 +51,55 @@ import           Universum
 
 main :: IO ()
 main = do
-  args@(Args (Sync path dst toWatch) conf) <- execParser arguments
-  canonPath                    <- canonicalizePath path
-  (stdo, stde, loggingCleanUp) <- initLogging
-  inputChan                    <- newChan
-  outputChan                   <- newChan
+  args <- execParser arguments
+  case args of
+    (Args (Sync path dst toWatch) conf) -> do
+      canonPath                    <- canonicalizePath path
+      (stdo, stde, loggingCleanUp) <- initLogging
+      inputChan                    <- newChan
+      outputChan                   <- newChan
 
-  let loggers   = (stdo, stde)
-      verbosity = configVerbosity conf
-      bootstrap = Bootstrapped conf loggers inputChan outputChan
-      send      = writeChan inputChan
-      debug     = logDebug' loggers verbosity
-      info      = logInfo' loggers verbosity
-      error'    = logError' loggers verbosity
-      term      = CatchOnce $ send SystemTerminatedEvent
+      let loggers   = (stdo, stde)
+          verbosity = configVerbosity conf
+          bootstrap = Bootstrapped conf loggers inputChan outputChan
+          send      = writeChan inputChan
+          debug     = logDebug' loggers verbosity
+          info      = logInfo' loggers verbosity
+          error'    = logError' loggers verbosity
+          term      = CatchOnce $ send SystemTerminatedEvent
 
-  _ <- installHandler sigTERM term Nothing
-  _ <- installHandler sigINT term Nothing
+      _ <- installHandler sigTERM term Nothing
+      _ <- installHandler sigINT term Nothing
 
-  debug $ "Arguments " <> show args
+      debug $ "Arguments " <> show args
 
-  send (SyncEvent path dst)
-  send EndEvent
+      send (SyncEvent path dst)
+      send EndEvent
 
-  _ <- forkIO . forever $ do
-    result <- liftIO $ runResult bootstrap execute
-    whenLeft result error'
+      _ <- forkIO . forever $ do
+        result <- liftIO $ runResult bootstrap execute
+        whenLeft result error'
 
-  unless toWatch $ send EndEvent
+      unless toWatch $ send EndEvent
 
-  watcher <- forkIO . when toWatch $ withManagerConf
-    defaultConfig
-    (\manager -> do
-      watchManagerCleanUp <- watchDir manager
-                                      (takeDirectory path)
-                                      (equalFilePath canonPath . eventPath)
-                                      (const $ send (SyncEvent path dst))
-      let stop = watchManagerCleanUp >> debug "File watcher was stopped"
-      handle @AsyncException (const stop) $ do
-        info "📝 Listening for changes... Press any key to stop"
-        line <- getLine
-        stop
-        send $ UserTerminatedEvent line
-    )
+      watcher <- forkIO . when toWatch $ withManagerConf
+        defaultConfig
+        (\manager -> do
+          watchManagerCleanUp <- watchDir
+            manager
+            (takeDirectory path)
+            (equalFilePath canonPath . eventPath)
+            (const $ send (SyncEvent path dst))
+          let stop = watchManagerCleanUp >> debug "File watcher was stopped"
+          handle @AsyncException (const stop) $ do
+            info "📝 Listening for changes... Press any key to stop"
+            line <- getLine
+            stop
+            send $ UserTerminatedEvent line
+        )
+      readChan outputChan
+      killThread watcher
+      debug "Logging handlers cleaned up"
+      loggingCleanUp
 
-  readChan outputChan
-  killThread watcher
-  debug "Logging handlers cleaned up"
-  loggingCleanUp
+    (Args Version _) -> putStrLn "ORG2ANY_VERSION"
